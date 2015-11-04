6ff79c14ec20c260820fb1ab96b749dbe577ae47
Christoffer Vikström <cvi@campanja.com>
2015-11-04 09:32:12 +0100

    Add an in-memory queue bridge
    * Modify the print_worker to use the the mem_bridge for testing

--- test/print_worker.erl
+++ test/print_worker.erl
@@ -2,17 +2,17 @@
 
 -behaviour(gen_eqw).
 
--export([init/1, handle_msg/2]).
+-export([init/1, handle_msg/3]).
 
--export([start/1, start/3, stop/1]).
+-export([start/0, start/2, stop/1]).
 
-start(QueueName) ->
-    start(QueueName, 10, 10).
+start() ->
+    start(2, 10).
 
-start(QueueName, Pollers, Workers) ->
+start(Pollers, Workers) ->
     eqw:start(),
     Opts = #{num_pollers => Pollers, max_workers => Workers},
-    eqw:add_pool(sqs, #{queue_name => QueueName}, ?MODULE, no_args, Opts).
+    eqw:add_pool(mem_bridge, no_args, ?MODULE, no_args, Opts).
 
 stop(Ref) ->
     eqw:del_pool(Ref).
@@ -20,6 +20,6 @@ stop(Ref) ->
 init(no_args) ->
     {ok, #{}}.
 
-handle_msg(Msg, #{}) ->
+handle_msg(Msg, _, #{}) ->
     io:format("msg: ~p~n~n", [Msg]),
     ok.
