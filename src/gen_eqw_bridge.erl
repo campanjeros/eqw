7721c5543ab498fa4d0cfb008a217d5972b15b32
mats cronqvist <masse@klarna.com>
2016-06-15 10:01:45 +0200

    add 'metadata' callback to behaviour

--- src/gen_eqw_bridge.erl
+++ src/gen_eqw_bridge.erl
@@ -19,3 +19,12 @@
 
 %% Should wrap a send-batch call for the specific queue-system
 -callback send([Msg::any()], State::any()) -> ok | {error, Reason::any()}.
+
+%% returns queue system metadata
+%% return value is a map with two keys; 'request_queue' and 'response_queue'.
+%% both map values are maps. The keys 'name', 'length' and 'handling' are
+%% mandatory, but there can be more fields if appropriate for the queue system.
+%% Value of 'name' is a string, length (number of messages in the queue) is an
+%% integer, and 'handling' (the number of messages that has been received but
+%% not yet deleted) is an integer.
+-callback metadata(State::any()) -> map().
